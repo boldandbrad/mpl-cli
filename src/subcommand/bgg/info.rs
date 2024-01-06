101c94d63c04de4131a7d4d92de2ca9cb5c33e41
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-01-06 12:10:45 -0500

    first pass at bgg info

--- src/subcommand/bgg/info.rs
+++ src/subcommand/bgg/info.rs
@@ -3,5 +3,18 @@ use anyhow::Result;
 
 pub fn info(bgg_id: String) -> Result<()> {
     let response = get_item(bgg_id);
-    Ok(println!("{:?}", response))
+    match response {
+        Ok(title) => {
+            // print out title info
+            println!("{}\t{} ({})", title.id, title.name, title.year);
+            println!("  Rating:\tNA");
+            println!("  Rank: \tNA");
+            println!("  Weight:\tNA");
+            println!("  Players:\t{}-{}", title.min_players, title.max_players);
+            println!("  Play Time:\t~{} Min", title.play_time);
+            println!("  Min Age:\t{}", title.min_age);
+            Ok(())
+        }
+        Err(e) => Err(e),
+    }
 }
